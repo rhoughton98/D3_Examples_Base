73b844709cf67d850c9d05d92bfe5e35287de900
Cody Dunne <cody.g.dunne@gmail.com>
2022-02-04 13:47:42 -0500

    Initial commit

---
+++ 6-transition/main.js
@@ -0,0 +1,87 @@
+//Define data
+let data = [
+  { name: 'John'    , rating: 5 },
+  { name: 'Arya'    , rating: 6 },
+  { name: 'Jamie'   , rating: 7 },
+  { name: 'Daenerys', rating: 9 },
+  { name: 'Cersei'  , rating: 2 },
+  { name: 'Ramsay'  , rating: 4 }
+];
+
+// Create SVG
+let
+  width = 600,
+  height = 400;
+  
+let margin = {
+  top: 40,
+  bottom: 30,
+  left: 30,
+  right: 30
+};
+
+let svg = d3
+  .select('body')
+  .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .style('background', '#e9f7f2');
+
+// Define Scales
+let yScale = d3.scaleLinear()
+  .domain([0, 10])
+  .range([height - margin.bottom, margin.top]);
+
+let xScale = d3.scaleBand()
+  .domain(
+    data.map(function(d) {
+      return d.name;
+    })
+  )
+  .range([margin.left, width - margin.right])
+  .padding(0.5);
+
+//Draw Axes
+let yAxis = svg
+  .append('g')
+    .attr('transform', `translate(${margin.left},0)`)
+    .call(d3.axisLeft().scale(yScale));
+
+//Add label
+yAxis
+  .append('text')
+    .attr('y', 30)
+    .attr('x', 20)
+    .style('stroke', 'black')
+    .text('Rating');
+
+let xAxis = svg
+  .append('g')
+    .attr('transform', `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom().scale(xScale));
+  
+//Add label
+xAxis
+  .append('text')
+    .attr('x', width - margin.left)
+    .attr('y', -10)
+    .style('stroke', 'black')
+    .text('Names');
+
+//Draw bars
+let bar = svg
+  .selectAll('rect')
+    .data(data)
+  .enter()
+  .append('rect')
+    .attr('x', function(d) {
+      return xScale(d.name);
+    })
+    .attr('y', function(d) {
+      return yScale(d.rating);
+    })
+    .attr('width', xScale.bandwidth())
+    .attr('fill', 'steelblue')
+    .attr('height', function(d) {
+      return height - margin.bottom - yScale(d.rating);
+    });
---
+++ README.md
@@ -0,0 +1,3 @@
+# D3 Examples
+
+This repository holds the examples from our in-class D3 tutorial.
